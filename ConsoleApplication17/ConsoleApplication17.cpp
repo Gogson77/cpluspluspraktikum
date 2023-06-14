34310d1702888605c7b53be638074294e418a84f
Gordan Kranjcic <gordankranjcic@gmail.com>
2023-06-14 13:38:48 +0200

    rjesenje

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned int a = 5;
+	cout << a << endl;
+
+	a = -5;
+	cout << a << endl;
+
+	return 0;
+}
---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned short int maliBroj;
+	maliBroj = 65535;
+	cout << "Mali broj: " << maliBroj << endl;
+
+	maliBroj++;
+	cout << "Mali broj: " << maliBroj << endl;
+
+	maliBroj++;
+	cout << "Mali broj: " << maliBroj << endl;
+
+	return 0;
+}
