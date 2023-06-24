03a2172bf1c80ac819707f173b8a0c668fc7fa08
Gordan Kranjcic <gordankranjcic@gmail.com>
2023-06-24 23:32:50 +0200

    rjesenje string

--- ConsoleApplication35/ConsoleApplication35.cpp
+++ ConsoleApplication35/ConsoleApplication35.cpp
@@ -1,7 +1,61 @@
 ﻿#include <iostream>
 #include <string>
-#include "kTakmicenje.h"
+//#include <string.h>
 using namespace std;
+
+typedef enum eStimulans { Da, Ne };
+
+class kTakmicenje
+{
+public:
+	string pUcesnik;
+	int pMjesto;
+	int pOsnova;
+	float pVrijednost;
+	eStimulans pDoping;
+	kTakmicenje(
+		string pU,
+		int pM,
+		int pO,
+		float pV,
+		eStimulans pD
+	);
+	int fNagrada();
+	~kTakmicenje();
+};
+
+kTakmicenje::kTakmicenje(
+	string pU,
+	int pM,
+	int pO,
+	float pV,
+	eStimulans pD
+) {
+	pUcesnik = pU;
+	pMjesto = pM;
+	pOsnova = pO;
+	pVrijednost = pV;
+	pDoping = pD;
+};
+
+int kTakmicenje::fNagrada() {
+	float pNagrada;
+	cout << "UCESNIK: " << endl;
+	cout << pUcesnik << endl;
+	cout << "Doping kontrola je " << pDoping << endl;
+	cout << "Osnova je " << pOsnova << endl;
+	pNagrada = (pMjesto == 1 && pDoping != Da)
+		? (++pOsnova) * pVrijednost
+		: (pOsnova++) * pVrijednost;
+	cout << "Nagrada je " << pNagrada << " dinara." << endl;
+	cout << "Nova osnova je " << pOsnova << endl << endl;
+	return 0;
+};
+
+kTakmicenje::~kTakmicenje() {
+	cout << "Takmicenje je zavrseno!" << endl << endl;
+};
+
 
 int main()
 {
